46a60a93c715b64bc69a09d5cfcd5dd901fe865d
Yashu <cyashu2000@gmail.com>
2024-06-29 20:40:31 +0530

    Added new GUID

--- api/welcome.js
+++ api/welcome.js
@@ -1,8 +1,12 @@
+const Guid = require('guid');
+
 class Welcome{
     static async getWelcomeMessage(event, callback) {
         console.log('Enter Welcome - Welcome message - event body', event.body, typeof event.body);
+
         try {
-            var welcomeMessage = "Hello!! Welcome to Yashu's server - testing CI/CD with this get request V5.0"
+            var guid = Guid.create();
+            var welcomeMessage = `Hello!! Welcome to Yashu's server - testing CI/CD with this get request V6.0. This GUID is added to test new dependencies installation through CI/CD ${guid}`
             console.log('Exit Welcome - Welcome message: ', welcomeMessage);
             callback(null, {
                 welcomeMessage
