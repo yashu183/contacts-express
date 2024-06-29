b14613e5d5dc0ec5c30f84ffaa0b7b4404a21b5c
Yashu <cyashu2000@gmail.com>
2024-06-29 19:44:59 +0530

    added deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,6 @@
+echo "Connected!!!"
+cd /home/yashu/public_html/contacts.yashu.fun/
+ls -al
+mkdir /home/yashu/public_html/contacts.yashu.fun/tmp
+touch /home/yashu/public_html/contacts.yashu.fun/tmp/restart.txt
+ls /home/yashu/public_html/contacts.yashu.fun/tmp
